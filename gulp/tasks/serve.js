ce7e6e08a1696239cba783fbdbe05f6c07ce4bef
Neil <Neil>
2014-09-12 10:09:22 +0100

    Add connect-prism for proxying/mocks/recording

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -2,22 +2,51 @@
 
 var config = require('../config');
 var gulp = require('gulp');
+var connect = require('connect');
+var url = require('url');
+var prism = require('connect-prism');
+
+
+function prismInit(mode) {
+
+    prism.create({
+        name: 'api',
+        mode: mode || 'proxy',
+        context: '/api',
+        host: 'localhost',
+        port: '2676'
+    });
+
+}
 
 // Connect
 gulp.task('connect', function () {
-  var connect = require('connect');
-  var app = connect()
-    .use(require('connect-livereload')({ port: config.livereloadPort }))
-    .use('/', connect.static('.tmp'))
-    .use('/bower_components', connect.static('bower_components'))
 
-  require('http').createServer(app)
-    .listen(config.port)
-    .on('listening', function () {
-      console.log('Started connect web server on http://localhost:' + config.port);
-    });
+    var app = connect()
+        .use(require('connect-livereload')({ port: config.livereloadPort }))
+        .use('/', connect.static('.tmp'))
+        .use('/bower_components', connect.static('bower_components'))
+        .use(prism.middleware);
+
+    require('http').createServer(app)
+        .listen(config.port)
+        .on('listening', function () {
+            console.log('Started connect web server on http://localhost:' + config.port);
+        });
 });
 
 gulp.task('serve', ['connect', 'styles'], function () {
-  //require('opn')('http://localhost:' + config.port);
+    prismInit();
+});
+
+gulp.task('serve:mock', ['connect', 'styles'], function () {
+    prismInit('mock');
+});
+
+gulp.task('serve:record', ['connect', 'styles'], function () {
+    prismInit('record');
+});
+
+gulp.task('serve:proxy', ['connect', 'styles'], function () {
+    prismInit('proxy');
 });
